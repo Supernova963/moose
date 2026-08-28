2d1ed8dedc84925eed3baf35da9c1b4d543885b1
Christian Mikhail Robinette <christian.robinette@inl.gov>
2026-08-28 14:11:44 -0600

    Changed ADKKSMultiBulkBase to make dhj/detai = 0 when hj is not dependent on etai

--- modules/phase_field/src/kernels/ADKKSMultiACBulkBase.C
+++ modules/phase_field/src/kernels/ADKKSMultiACBulkBase.C
@@ -12,21 +12,21 @@
 InputParameters
 ADKKSMultiACBulkBase::validParams()
 {
-    InputParameters params = ADAllenCahnBase<Real>::validParams();
-    params.addClassDescription("Multi-order parameter KKS model kernel for the Bulk Allen-Cahn. This "
-                               "operates on one of the order parameters 'eta_i' as the non-linear "
-                               "variable");
-    params.addRequiredParam<std::vector<MaterialPropertyName>>(
-        "Fj_names", "List of free energies for each phase. Place in same order as hj_names!");
-    params.addRequiredParam<std::vector<MaterialPropertyName>>(
-        "hj_names", "Switching Function Materials that provide h. Place in same order as Fj_names!");
-    params.addRequiredCoupledVar("eta_i",
-                                 "Order parameter that derivatives are taken with respect to");
-    return params;
+  InputParameters params = ADAllenCahnBase<Real>::validParams();
+  params.addClassDescription("Multi-order parameter KKS model kernel for the Bulk Allen-Cahn. This "
+                             "operates on one of the order parameters 'eta_i' as the non-linear "
+                             "variable");
+  params.addRequiredParam<std::vector<MaterialPropertyName>>(
+      "Fj_names", "List of free energies for each phase. Place in same order as hj_names!");
+  params.addRequiredParam<std::vector<MaterialPropertyName>>(
+      "hj_names", "Switching Function Materials that provide h. Place in same order as Fj_names!");
+  params.addRequiredCoupledVar("eta_i",
+                               "Order parameter that derivatives are taken with respect to");
+  return params;
 }
 
 ADKKSMultiACBulkBase::ADKKSMultiACBulkBase(const InputParameters & parameters)
-    : ADAllenCahnBase<Real>(parameters),
+  : ADAllenCahnBase<Real>(parameters),
     _etai_name(coupledName("eta_i", 0)),
     _Fj_names(getParam<std::vector<MaterialPropertyName>>("Fj_names")),
     _num_j(_Fj_names.size()),
@@ -34,26 +34,24 @@ ADKKSMultiACBulkBase::ADKKSMultiACBulkBase(const InputParameters & parameters)
     _hj_names(getParam<std::vector<MaterialPropertyName>>("hj_names")),
     _prop_dhjdetai(_num_j)
 {
-    if (_num_j != _hj_names.size())
-        paramError("hj_names", "Need to pass in as many hj_names as Fj_names");
-    
-    for (unsigned int n = 0; n < _num_j; ++n)
-    {
-        _prop_Fj[n] = &getADMaterialPropertyByName<Real>(_Fj_names[n]);
+  if (_num_j != _hj_names.size())
+    paramError("hj_names", "Need to pass in as many hj_names as Fj_names");
 
-        _prop_dhjdetai[n] = &getADMaterialPropertyByName<Real>(
-                            derivativePropertyNameFirst(_hj_names[n], _etai_name));
-    }
+  for (unsigned int n = 0; n < _num_j; ++n)
+  {
+    _prop_Fj[n] = &getADMaterialPropertyByName<Real>(_Fj_names[n]);
+    _prop_dhjdetai[n] = &getMaterialPropertyDerivativeByName<Real, true>(_hj_names[n], _etai_name);
+  }
 }
 
 void
 ADKKSMultiACBulkBase::initialSetup()
 {
-    ADAllenCahnBase<Real>::initialSetup();
+  ADAllenCahnBase<Real>::initialSetup();
 
-    for (unsigned int n = 0; n < _num_j; ++n)
-    {
-        validateNonlinearCoupling<ADReal>(_Fj_names[n]);
-        validateNonlinearCoupling<ADReal>(_hj_names[n]);
-    }
+  for (unsigned int n = 0; n < _num_j; ++n)
+  {
+    validateNonlinearCoupling<ADReal>(_Fj_names[n]);
+    validateNonlinearCoupling<ADReal>(_hj_names[n]);
+  }
 }
